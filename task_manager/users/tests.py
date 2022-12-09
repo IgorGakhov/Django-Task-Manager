4ddefa6f464035a2305713f6b2b9a31851c6b1b6
Igor Gakhov <gakhov_igor@rambler.ru>
2022-12-09 20:52:30 +0300

    Experimenting with the environment setting

--- task_manager/users/tests.py
+++ task_manager/users/tests.py
@@ -5,7 +5,6 @@ from .constants import REVERSE_USERS
 
 
 class UsersTest(TestCase):
-
     def test_users_list(self) -> None:
         response: HttpResponse = self.client.get(REVERSE_USERS)
         self.assertTemplateUsed(response, template_name='users/users_list.html')
