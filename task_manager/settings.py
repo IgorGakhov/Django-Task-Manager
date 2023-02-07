edd373b2accf63a719fa4dc30872e8e83caec149
Igor Gakhov <gakhov_igor@rambler.ru>
2023-02-07 10:46:46 +0300

    Set default values for production DB data

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -101,11 +101,11 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 
 PRODUCTION_DATABASE = {
     'ENGINE': 'django.db.backends.postgresql',
-    'NAME': os.getenv('PGDATABASE'),
-    'USER': os.getenv('PGUSER'),
-    'PASSWORD': os.getenv('PGPASSWORD'),
-    'HOST': os.getenv('PGHOST'),
-    'PORT': os.getenv('PGPORT'),
+    'NAME': os.getenv('PGDATABASE', ''),
+    'USER': os.getenv('PGUSER', ''),
+    'PASSWORD': os.getenv('PGPASSWORD', ''),
+    'HOST': os.getenv('PGHOST', ''),
+    'PORT': os.getenv('PGPORT', ''),
 }
 
 DEVELOPMENT_DATABASE = {
