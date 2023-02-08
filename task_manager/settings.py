14d5ff35a13353a577bc91cb356f30c822c1df0f
Igor Gakhov <gakhov_igor@rambler.ru>
2023-02-08 11:50:30 +0300

    Change internationalization settings

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -150,9 +150,9 @@ rollbar.init(**ROLLBAR)
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
