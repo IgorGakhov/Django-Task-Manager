6e5957705f2189e4b2e701d3a700925526d33285
Igor Gakhov <gakhov_igor@rambler.ru>
2023-02-10 15:19:28 +0300

    Fix PK typing in users app URLPattern

--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -8,6 +8,6 @@ from .constants import LIST_USERS, CREATE_USER, UPDATE_USER, DELETE_USER
 urlpatterns: List[URLPattern] = [
     path('', UsersListView.as_view(), name=LIST_USERS),
     path('create/', UserCreateView.as_view(), name=CREATE_USER),
-    path('<pk>/update/', UserUpdateView.as_view(), name=UPDATE_USER),
-    path('<pk>/delete/', UserDeleteView.as_view(), name=DELETE_USER)
+    path('<int:pk>/update/', UserUpdateView.as_view(), name=UPDATE_USER),
+    path('<int:pk>/delete/', UserDeleteView.as_view(), name=DELETE_USER)
 ]
